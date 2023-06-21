f607c0a3d9fbda3fb93d1e74bff05bdebf558420
ptd3v <102877038+ptd3v@users.noreply.github.com>
2023-06-21 15:24:30 +0100

    API Update

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
-//Use the 'weather-container in the HTMLdocument.
 const weatherContainer = document.getElementById("weather-container");
 
 // Function to fetch weather data
 function fetchWeatherData() {
   const apiKey = "4a4cfe25b96cd02848b8c26cda238f6f";
-  const location = "London,UK";
-  const url = 'api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=4a4cfe25b96cd02848b8c26cda238f6f';
+  const location = "London,uk";
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=${apiKey}`;
 
   fetch(url)
     .then(response => response.json())
     .then(data => {
-      const temperature = data.current.temp_c;
-      const condition = data.current.condition.text;
+      const temperature = Math.round(data.main.temp - 273.15);
+      const condition = data.weather[0].description;
 
-      const weatherInfo = 'Temperature: ${temperature}°C<br>Condition: ${condition}';
+      const weatherInfo = `Temperature: ${temperature}°C<br>Condition: ${condition}`;
       weatherContainer.innerHTML = weatherInfo;
     })
     .catch(error => {
